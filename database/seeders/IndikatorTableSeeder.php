161b9e0d0ff13981fa6ef6b051ff4ec257c547ed
amirulhidayah <hidayahamirul103@gmail.com>
2022-07-16 10:33:05 +0800

    Ubah Seeder

--- database/seeders/IndikatorTableSeeder.php
+++ database/seeders/IndikatorTableSeeder.php
@@ -14,53 +14,51 @@ class IndikatorTableSeeder extends Seeder
      */
     public function run()
     {
-        
+
 
         \DB::table('indikator')->delete();
-        
-        \DB::table('indikator')->insert(array (
-            0 => 
-            array (
+
+        \DB::table('indikator')->insert(array(
+            0 =>
+            array(
                 'id' => '50957899-4fe5-456e-9e62-a5573d85f59b',
-                'nama' => 'Memberikan bantuan sembako',
+                'nama' => 'Mengecek Kondisi Kolam Warga',
                 'deleted_at' => NULL,
                 'created_at' => '2022-05-19 13:57:18',
                 'updated_at' => '2022-05-19 13:57:18',
             ),
-            1 => 
-            array (
+            1 =>
+            array(
                 'id' => '5ad88039-728b-4ea9-b3c9-b724d3dcbdb8',
-                'nama' => 'Pemasangan Rambu-rambu jalur evakuasi',
+                'nama' => 'Edukasi Mengenai Schistosomiasis',
                 'deleted_at' => NULL,
                 'created_at' => '2022-05-19 13:58:09',
                 'updated_at' => '2022-05-19 13:58:09',
             ),
-            2 => 
-            array (
+            2 =>
+            array(
                 'id' => 'a8b0edf8-2437-427f-9075-97d8a9fb6ce5',
-                'nama' => 'Pembangunan MCK',
+                'nama' => 'Pencegahan Schistosomiasis',
                 'deleted_at' => NULL,
                 'created_at' => '2022-05-19 13:57:39',
                 'updated_at' => '2022-05-19 13:57:39',
             ),
-            3 => 
-            array (
+            3 =>
+            array(
                 'id' => 'c72b5a8e-a05c-4aa4-919d-8a28d908e287',
-                'nama' => 'Edukasi Cara Membayar Pajak Via Sosial Media',
+                'nama' => 'Pemeriksaan Sawah Warga',
                 'deleted_at' => NULL,
                 'created_at' => '2022-05-19 13:56:14',
                 'updated_at' => '2022-05-19 13:56:14',
             ),
-            4 => 
-            array (
+            4 =>
+            array(
                 'id' => 'ef25a5b2-43ee-4689-bc3a-f2c830da197c',
-                'nama' => 'Pembagian Masker',
+                'nama' => 'Pendataan Warga',
                 'deleted_at' => NULL,
                 'created_at' => '2022-05-19 13:56:29',
                 'updated_at' => '2022-05-19 13:56:29',
             ),
         ));
-        
-        
     }
 }
